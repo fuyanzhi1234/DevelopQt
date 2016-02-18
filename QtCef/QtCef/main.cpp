8bd36ca153b1999f399e845c1e0d7d47f0885a74
fuyanzhi <willing4567@163.com>
2016-02-18 17:40:29 +0800

    delete unnecessary files

--- QtCef/QtCef/main.cpp
+++ QtCef/QtCef/main.cpp
@@ -4,20 +4,10 @@
 #include "include/base/cef_scoped_ptr.h"
 #include "include/cef_command_line.h"
 #include "include/cef_sandbox_win.h"
-#include "cefclient/browser/client_app_browser.h"
-#include "cefclient/browser/main_context_impl.h"
-#include "cefclient/browser/main_message_loop_multithreaded_win.h"
-#include "cefclient/browser/main_message_loop_std.h"
-#include "cefclient/browser/root_window_manager.h"
-#include "cefclient/browser/test_runner.h"
-#include "cefclient/common/client_app_other.h"
-#include "cefclient/renderer/client_app_renderer.h"
 
 #include "cefsimple/simple_app.h"
 #include "include/cef_sandbox_win.h"
 #include <QMessageBox>
-
-using namespace client;
 
 int main(int argc, char *argv[])
 {
